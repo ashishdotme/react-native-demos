6397409004e2e66700f76f77a04beaaf9c91cf9a
Ashish Patel <ashishsushilpatel@gmail.com>
2017-03-16 02:33:41 +0530

    Refactor Movie Search app

--- MovieSearchApp/app/Movie.js
+++ MovieSearchApp/app/Movie.js
@@ -5,13 +5,12 @@ import {
   Text,
   View,
   Image,
-  TouchableHighlight,
   ListView,
-  ScrollView,
   ActivityIndicator,
   TextInput,
   Alert
 } from 'react-native';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -70,8 +69,26 @@ const styles = StyleSheet.create({
     left: 120,
     width: 100,
     height: 100
+  },
+  imgsList: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 0,
+    alignItems: 'center',
+    borderColor: '#D7D7D7',
+    borderBottomWidth: 1,
+    backgroundColor: '#fff',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  img: {
+    height: 95,
+    width: 75,
+    borderRadius: 20,
+    margin: 20
   }
 });
+
 export default class MovieSearchApp extends Component {
   constructor(props) {
     super(props);
@@ -80,62 +97,61 @@ export default class MovieSearchApp extends Component {
     });
     this.state = {
       showProgress: false,
-      dataSource: ds.cloneWithRows(['Ashish', 'Patel'])
+      dataSource: ds.cloneWithRows([]),
+      loaded: false
     };
     this.searchMovies = this.searchMovies.bind(this);
   }
+
   searchMovies() {
-    console.log('Ashish');
     this.setState({showProgress: true});
     fetch(`https://www.omdbapi.com/?s=${this.state.searchText}`).then(response => response.json()).then((responseData) => {
-      console.log(responseData.Search);
       this.fetchDescription(responseData.Search)
     }).catch((error) => {
       Alert.alert("Warning", "Not able to find !");
     }).done()
   }
+
   fetchDescription(data) {
-    var plotArray = [];
+    var moviesArray = [];
     for (var i = 0; i < data.length; i++) {
       let url = 'https://www.omdbapi.com/?i=' + data[i].imdbID + '&plot=short&r=json';
       fetch(url).then(response => response.json()).then((responseData) => {
-        this.setState({
-          plot: [responseData.Poster, responseData.Year, responseData.Title, responseData.Plot]
-        });
-        console.log(this.state.plot);
-        plotArray.push(this.state.plot);
-        this.setState({showProgress: false});
-        this.setState({dataSource: this.state.dataSource.cloneWithRows(plotArray)});
+        this.setState({movie: responseData});
+        moviesArray.push(this.state.movie);
+        this.setState({dataSource: this.state.dataSource.cloneWithRows(moviesArray)});
       }).catch((error) => {
         Alert.alert("Warning", "Not able to find !");
       }).done()
     }
-    console.log(plotArray);
+    this.setState({showProgress: false});
+    this.setState({loaded: true})
   }
-  renderRow(rowData, section, row) {
+
+  renderRow(rowData) {
     return (
-      <View style={styles.row}>
+      <View style={styles.imgsList}>
         <Image
-          style={styles.poster}
           source={{
-          uri: rowData[0]
-        }}/>
-        <View style={{
-          flexDirection: 'column'
+          uri: rowData.Poster
+        }}
+          style={styles.img}/>
+        <View
+          style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'space-between'
         }}>
-          <Text style={styles.movieyear}>
-            {rowData[1]}
-          </Text>
-          <Text style={styles.movietitle}>
-            {rowData[2]}
-          </Text>
-          <Text style={styles.movietitle}>
-            {rowData[3]}
-          </Text>
+          <Text>{rowData.Year}</Text>
+          <Text style={{
+            fontWeight: 'bold'
+          }}>{rowData.Title}</Text>
+          <Text>{rowData.Plot}</Text>
         </View>
       </View>
     );
   }
+
   render() {
     return (
       <View style={styles.container}>
@@ -146,9 +162,11 @@ export default class MovieSearchApp extends Component {
           onChangeText={text => this.setState({searchText: text})}
           placeholder="Enter search keyword"
           onSubmitEditing={this.searchMovies}/>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this.renderRow}/>
+          { this.state.loaded &&
+            < ListView
+            dataSource = { this.state.dataSource }
+            renderRow = { this.renderRow } />
+            }
         <ActivityIndicator
           style={styles.progress}
           animating={this.state.showProgress}
@@ -157,4 +175,5 @@ export default class MovieSearchApp extends Component {
     );
   }
 }
-AppRegistry.registerComponent('MovieSearchApp', () => MovieSearchApp);;
+
+AppRegistry.registerComponent('MovieSearchApp', () => MovieSearchApp);
