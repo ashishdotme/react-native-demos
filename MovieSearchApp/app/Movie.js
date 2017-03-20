6a346eb69eb886727ca55b19dbe08bb2fe05266e
Ashish Patel <ashishsushilpatel@gmail.com>
2017-03-20 17:38:52 +0530

    Added movie details screen

--- MovieSearchApp/app/Movie.js
+++ MovieSearchApp/app/Movie.js
@@ -9,87 +9,30 @@ import {
   ActivityIndicator,
   TextInput,
   Alert,
+  TouchableOpacity,
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    marginLeft: 20,
-    marginRight: 20,
-    marginTop: 40,
-  },
-  header: {
-    fontSize: 24,
-    alignSelf: 'center',
-    marginBottom: 10,
-  },
-  searchInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingLeft: 10,
-    marginBottom: 10,
-  },
-  row: {
-    height: 150,
-    flexDirection: 'row',
-  },
-  contact: {
-    fontSize: 24,
-  },
-  listItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    margin: 5,
-  },
-  poster: {
-    marginTop: 20,
-    height: 75,
-    width: 50,
-  },
-  title: {
-    margin: 5,
-    fontSize: 15,
-  },
-  subHeading: {
-    margin: 5,
-    fontSize: 12,
-  },
-  movietitle: {
-    marginLeft: 10,
-  },
-  movieyear: {
-    marginTop: 20,
-    marginLeft: 10,
-  },
-  progress: {
-    position: 'absolute',
-    top: 230,
-    left: 120,
-    width: 100,
-    height: 100,
-  },
-  imgsList: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 0,
-    alignItems: 'center',
-    borderColor: '#D7D7D7',
-    borderBottomWidth: 1,
-    backgroundColor: '#fff',
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  img: {
-    height: 95,
-    width: 75,
-    borderRadius: 20,
-    margin: 20,
-  },
-});
+import {
+  StackNavigator,
+} from 'react-navigation';
 
-export default class MovieSearchApp extends Component {
+import MovieDetail from './MovieDetail';
+
+class SearchMovie extends Component {
+  static propTypes = {
+    navigation: React.PropTypes.object.isRequired,
+  }
+
+  static navigationOptions = {
+    title: 'Movies',
+    header: {
+      style: {
+        backgroundColor: '#1a91cb',
+      },
+      tintColor: 'white',
+    },
+  };
+
   constructor(props) {
     super(props);
     const ds = new ListView.DataSource({
@@ -103,79 +46,42 @@ export default class MovieSearchApp extends Component {
     this.searchMovies = this.searchMovies.bind(this);
   }
 
-  searchMovies() {
-    this.setState({ showProgress: true });
-    fetch(`https://www.omdbapi.com/?s=${this.state.searchText}`).then(response => response.json()).then((responseData) => {
-      this.fetchDescription(responseData.Search);
-    }).catch((error) => {
-      console.log(error);
-      Alert.alert('Warning', 'Not able to find !');
-    }).done();
-  }
-
-  fetchDescription(data) {
-    const moviesArray = [];
-    for (let i = 0; i < data.length; i++) {
-      const url = `https://www.omdbapi.com/?i=${data[i].imdbID}&plot=short&r=json`;
-      fetch(url).then(response => response.json()).then((responseData) => {
-        this.setState({ movie: responseData });
-        moviesArray.push(this.state.movie);
-        this.setState({ dataSource: this.state.dataSource.cloneWithRows(moviesArray) });
-      }).catch((error) => {
-        console.log(error);
-        Alert.alert('Warning', 'Not able to find !');
-      }).done()
-    }
-    this.setState({ loaded: true });
-    this.setState({ showProgress: false });
-  }
-
-  renderRow(rowData) {
-    return (
-      <View style={styles.imgsList}>
-        <Image
-          source={{
-            uri: rowData.Poster,
-          }}
-          style={styles.img}
-        />
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Text>{rowData.Year}</Text>
-          <Text>{rowData.Language}</Text>
-          <Text
-            style={{
-              fontWeight: 'bold',
-            }}
-          >{rowData.Title}</Text>
-          <Text>{rowData.Plot}</Text>
-        </View>
-      </View>
-    );
-  }
-
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Search Movies
         </Text>
         <TextInput
           style={styles.searchInput}
-          onChangeText={text => this.setState({ searchText: text })}
+          onChangeText={(text) => this.setState({ searchText: text })}
           placeholder="Enter search keyword"
           onSubmitEditing={this.searchMovies}
         />
         { this.state.loaded &&
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this.renderRow}
-        />
+          <ListView
+            enableEmptySections
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) =>
+              <TouchableOpacity onPress={() => navigate('Detail', {data: rowData})}>
+                <View style={styles.imgsList}>
+                  <Image
+                    source={{
+                      uri: rowData.Poster,
+                    }}
+                    style={styles.img}
+                  />
+                  <View style={styles.row}  >
+                    <Text>{rowData.Year}</Text>
+                    <Text>{rowData.Language}</Text>
+                    <Text style={styles.movieTitle}> {rowData.Title} </Text>
+                    <Text>{rowData.Plot}</Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
             }
+          />
+        }
         <ActivityIndicator
           style={styles.progress}
           animating={this.state.showProgress}
@@ -184,6 +90,91 @@ export default class MovieSearchApp extends Component {
       </View>
     );
   }
+
+  fetchDescription(data) {
+    const moviesArray = [];
+    for (let i = 0; i < data.length; i++) {
+      const url = `https://www.omdbapi.com/?i=${data[i].imdbID}&plot=short&r=json`;
+      fetch(url).then((response) => response.json()).then((responseData) => {
+        this.setState({ movie: responseData });
+        moviesArray.push(this.state.movie);
+        this.setState({ dataSource: this.state.dataSource.cloneWithRows(moviesArray) });
+      }).catch((error) => {
+        Alert.alert('Error', error.description);
+      }).done();
+    }
+    this.setState({ loaded: true });
+    this.setState({ showProgress: false });
+  }
+
+  searchMovies() {
+    this.setState({ showProgress: true });
+    fetch(`https://www.omdbapi.com/?s=${this.state.searchText}`).then((response) => response.json()).then((responseData) => {
+      this.fetchDescription(responseData.Search);
+    }).catch((error) => {
+      Alert.alert('Error', error);
+    }).done();
+  }
+
 }
+
+const $gray = '#A8A8A8', $lightgray = '#D7D7D7', $white = '#FFFFFF',
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      marginLeft: 20,
+      marginRight: 20,
+      marginTop: 20,
+    },
+    header: {
+      fontSize: 24,
+      alignSelf: 'center',
+      marginBottom: 10,
+    },
+    searchInput: {
+      height: 40,
+      borderColor: $gray,
+      borderWidth: 1,
+      paddingLeft: 10,
+      marginBottom: 10,
+    },
+    progress: {
+      position: 'absolute',
+      top: 230,
+      left: 120,
+      width: 100,
+      height: 100,
+    },
+    imgsList: {
+      flex: 1,
+      flexDirection: 'row',
+      padding: 0,
+      alignItems: 'center',
+      borderColor: $lightgray,
+      borderBottomWidth: 1,
+      backgroundColor: $white,
+      paddingTop: 10,
+      paddingBottom: 10,
+    },
+    img: {
+      height: 95,
+      width: 75,
+      borderRadius: 20,
+      margin: 20,
+    },
+    row:{
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+    movieTitle: {
+      fontWeight: 'bold',
+    },
+  }),
+  MovieSearchApp = StackNavigator({
+    Main: {screen: SearchMovie},
+    Detail: {screen: MovieDetail},
+  });
 
 AppRegistry.registerComponent('MovieSearchApp', () => MovieSearchApp);
